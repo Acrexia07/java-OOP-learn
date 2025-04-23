6b595493286696d004b9b4d075cca48da31e0deb
Acrexia07 <barnuevo.marlon07@gmail.com>
2025-04-23 18:24:33 +0800

    Initial Commit

---
+++ src/JavaOOP_GetterSetters/Car.java
@@ -0,0 +1,25 @@
+package JavaOOP_GetterSetters;
+
+public class Car {
+
+    private String model;
+    private String color;
+    private int price;
+
+    Car(String model, String color, int price){
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    String getModel(){
+        return this.model;
+    }
+    String getColor(){
+        return this.color;
+    }
+    String getPrice(){
+
+        return "$" + this.price;
+    }
+}
---
+++ src/JavaOOP_GetterSetters/Main.java
@@ -0,0 +1,17 @@
+package JavaOOP_GetterSetters;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // They help protect object data and add rules for accessing or modifying them
+        // GETTER = Methods that make a field READABLE
+        // SETTER = Methods that make a field WRITABLE
+
+        Car car = new Car ("Charger", "Yellow", 10000);
+
+
+
+        System.out.println(car.getModel() + " " + car.getColor() + " " + car.getPrice());
+    }
+}
